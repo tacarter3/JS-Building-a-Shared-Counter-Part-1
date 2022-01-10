1d5d6519fa3e6d3f3861adbb21e0e4da4d2b6d9b
Timothy Carter <talic2002@gmail.com>
2022-01-10 16:40:42 -0500

    changed font size on navbar

--- public/index.js
+++ public/index.js
@@ -1,9 +1,15 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh-button');
+    //let countValue = 0;
 
-    let countValue = 0;
+    const response = await fetch('http://localhost:9001/counter');
+
+    const result = await response.json();
+
+    let countValue = result.value;
 
     function increment(){
         countValue++;
@@ -15,8 +21,18 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    function refreshed(){
+        const initial = await fetch('http://localhost:9001/counter');
+        const final = await initial.json();
+        let countValue = final.value;
+        countContainer.textContent = countValue;
+    }
+
+    
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refreshed)
     countContainer.textContent = countValue;
 }
 main()
